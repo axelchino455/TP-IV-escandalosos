6923ffe65a3911c53f11d2c5de6cea6c3e46ae51
axelchino455 <axel.chino.et7@gmail.com>
2024-10-02 14:48:44 -0300

    versión 1.2

--- src/escandalosos/Main.java
+++ src/escandalosos/Main.java
@@ -6,18 +6,44 @@ import java.util.List;
 public class Main {
 
 	public static void main(String[] args) {
-		List<String> habilidadesPardo = new ArrayList;
+		// creando listas para que pasarlo como parametron en la clase Oso
+		List<String> habilidadesPardo = new ArrayList<>();
 		habilidadesPardo.add("rapido");
 		
-		List<String> habilidadesPanda = new ArrayList;
+		List<String> habilidadesPanda = new ArrayList<>();
 		habilidadesPanda.add("adorable");
 		
-		List<String> habilidadesPolar = new ArrayList;
-		habilidadesPolar.add("");
+		List<String> habilidadesPolar = new ArrayList<>();
+		habilidadesPolar.add("aikido");
+		habilidadesPolar.add("ninjutsu");
+		habilidadesPolar.add("taekwondo");
+		habilidadesPolar.add("karate");
+		habilidadesPolar.add("espionaje");
+		habilidadesPolar.add("muay thai");
+		habilidadesPolar.add("cocinero tradicional");
+		habilidadesPolar.add("cocinero Indú");
+		
+		// Creando la lista para intereses de los osos:
+		List<String> interecesPardo = new ArrayList<>();
+		interecesPardo.add("cuidad a sus hermanos");	
+		
+		
+		List<String> interecesPanda = new ArrayList<>();
+		interecesPanda.add("cultura pop");
+		interecesPanda.add("usar su telefono");
+		
+		List<String> interecesPolar = new ArrayList<>();
+		interecesPolar.add("el frio");
+		interecesPolar.add("cocinar");
+		interecesPolar.add("robotica");
+		
 		// Creando los objetos
-		Oso Pardo = new Oso("Pardo","oso pardo", "marron", "Carismatico y motivado", habilidadesPardo, true, "cuidad a sus hermanos", false);
-		Oso Panda = new Oso("Panda", "oso panda", "blaco con manchas negras", "timido", habilidadesPanda, false, "cultura pop, y su telefono", false);
-		Oso Polar = new Oso("Polar", "sos polar", "blaco", "callado", )
+		Oso Pardo = new Oso("Pardo","oso pardo", "marron", "Carismatico y motivado", habilidadesPardo, true, interecesPardo, false);
+		Oso Panda = new Oso("Panda", "oso panda", "blaco con manchas negras", "timido", habilidadesPanda, false, interecesPanda, false);
+		Oso Polar = new Oso("Polar", "oso polar", "blaco", "callado", habilidadesPolar, false, interecesPolar, false);
+		
+		// listo solo falta traer los datos correctos
+		System.out.print();
 	}
 
 }
--- src/escandalosos/Oso.java
+++ src/escandalosos/Oso.java
@@ -6,12 +6,12 @@ import java.util.List;
 public class Oso extends Animal{
 	private List<String> habilidades;
 	private boolean lider;
-	private String intereces;
+	private List<String> intereces;
 	private boolean novia;
 	
 	
 	public Oso(String nombre, String raza, String color, String perosnalidad, List<String> habilidades, boolean lider,
-			String intereces, boolean novia) {
+			List<String> intereces, boolean novia) {
 		super(nombre, raza, color, perosnalidad);
 		this.habilidades = habilidades;
 		this.lider = lider;
@@ -20,13 +20,13 @@ public class Oso extends Animal{
 	}
 
 
-	public List<String> mostrarHabilidad() {
+	public List<String> getHabilidades() {
 		return habilidades;
 	}
 
 
-	public void agregarHabilidad(List<String> habilidad) {
-		habilidades.addAll(habilidad);
+	public void setHabilidades(List<String> habilidades) {
+		this.habilidades = habilidades;
 	}
 
 
@@ -40,12 +40,12 @@ public class Oso extends Animal{
 	}
 
 
-	public String getIntereces() {
+	public List<String> getIntereces() {
 		return intereces;
 	}
 
 
-	public void setIntereces(String intereces) {
+	public void setIntereces(List<String> intereces) {
 		this.intereces = intereces;
 	}
 
@@ -58,6 +58,9 @@ public class Oso extends Animal{
 	public void setNovia(boolean novia) {
 		this.novia = novia;
 	}
+	
+	
+	
 	
 	
 	
